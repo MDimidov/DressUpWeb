ea4479b5851ba016714466aa191854143be45509
Mariyan <116071995+myro97@users.noreply.github.com>
2024-03-21 16:26:43 +0200

    Created DressUpWeb Project

---
+++ DressUpWeb/DressUpWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DressUpWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
